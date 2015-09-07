a150d4f8254afb3abeec69f87a5d0334928eb31e
lesguillemets <lesguillemets@users.noreply.github.com>
2015-09-07 23:53:28 +0900

    check with Tree

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,26 @@ import Haste
 import Haste.DOM
 import Haste.Graphics.Canvas
 import Turtle
+import LSystem
+
+toCommands :: String -> [Command]
+toCommands = map (\c -> case c of
+    'X' -> Stay
+    '+' -> Turn (pi/7)
+    '-' -> Turn (-pi/7)
+    '[' -> Save
+    ']' -> Load
+    'F' -> Draw 10
+    )
+treeSystem = LSystem {
+    _state = "X",
+    _rule = \c -> case c of
+        'X' -> "F-[[X]+X]+F[+FX]-X"
+        'F' -> "FF"
+        _ -> return c
+}
 
 main = do
     Just cnv <- getCanvasById "canv"
     drawFit cnv (RenderConfig (500,500) (20,20)) blankTurtle
-        . concat . replicate 8 $ [Turn (pi/13), Draw 20]
+        . toCommands . _state . (!!5) . iterate nextGen $ treeSystem
