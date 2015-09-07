0a2e5761dfc82c20f6b678680dce7d279ac6628e
lesguillemets <lesguillemets@users.noreply.github.com>
2015-09-08 00:05:15 +0900

    some play :)

--- src/Main.hs
+++ src/Main.hs
@@ -9,6 +9,8 @@ toCommands = map (\c -> case c of
     'X' -> Stay
     '+' -> Turn (pi/7)
     '-' -> Turn (-pi/7)
+    '*' -> Turn (pi/13)
+    '/' -> Turn (-pi/13)
     '[' -> Save
     ']' -> Load
     'F' -> Draw 10
@@ -16,12 +18,13 @@ toCommands = map (\c -> case c of
 treeSystem = LSystem {
     _state = "X",
     _rule = \c -> case c of
-        'X' -> "F-[[X]+X]+F[+FX]-X"
+        'X' -> "F-[[X]+X]+F*[[FX]F/][*FX]/X"
         'F' -> "FF"
         _ -> return c
 }
 
 main = do
     Just cnv <- getCanvasById "canv"
+    render cnv (color (RGB 255 255 255) . fill $ rect (0,0) (500,500))
     drawFit cnv (RenderConfig (500,500) (20,20)) blankTurtle
         . toCommands . _state . (!!5) . iterate nextGen $ treeSystem
