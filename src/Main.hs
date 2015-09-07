da251ebbc55c7b8850747a8f60f01f98da21b779
lesguillemets <lesguillemets@users.noreply.github.com>
2015-09-08 02:09:21 +0900

    simple performance profiling

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 import Haste
 import Haste.DOM
+import Haste.Performance
 import Haste.Graphics.Canvas
 import Turtle
 import LSystem
@@ -16,5 +17,8 @@ treeSystem = LSystem {
 main = do
     Just cnv <- getCanvasById "canv"
     render cnv (color (RGB 255 255 255) . fill $ rect (0,0) (500,500))
+    t0 <- now
     drawFit cnv (RenderConfig (500,500) (20,20)) blankTurtle
         . map defaultTranslator . _state . (!!6) . iterate nextGen $ treeSystem
+    t1 <- now
+    writeLog $ "Took " ++ show (t1 - t0) ++ " ms to calc and render."
