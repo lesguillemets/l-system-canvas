254597551435fa6cbcd35528062eb42d40fc467b
lesguillemets <lesguillemets@users.noreply.github.com>
2015-09-07 23:26:30 +0900

    LSystem - basics

--- src/LSystem.hs
+++ src/LSystem.hs
@@ -1,1 +1,9 @@
 module LSystem where
+
+data LSystem a = LSystem {
+    _state :: [a],
+    _rule :: a -> [a]
+}
+
+nextGen :: LSystem a -> LSystem a
+nextGen s = s {_state = concatMap (_rule s) (_state s)}
