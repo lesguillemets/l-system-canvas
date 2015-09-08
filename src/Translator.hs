f4132aee3c787ae90f30e2e11e7448f6f73b98e5
lesguillemets <lesguillemets@users.noreply.github.com>
2015-09-08 11:08:25 +0900

    Add {} as a save/load pair

--- src/Translator.hs
+++ src/Translator.hs
@@ -7,6 +7,7 @@ type Translator = (Char -> Command)
 defaultMap :: M.Map Char Command
 defaultMap = M.fromList $
     [ ('[', Save), (']', Load)]
+    ++ [ ('{', Save), ('}', Load)]
     ++ zip ['1'..'9'] (map (Draw . fromIntegral . (* 5)) ([1..9]::[Int]))
     ++ zip ['a'..'z'] (map (Turn . (\n -> pi/n) . fromIntegral) ([1..]::[Int]))
     ++ zip ['A'..'Z'] (map (Turn . (\n -> -pi/n) . fromIntegral) ([1..]::[Int]))
